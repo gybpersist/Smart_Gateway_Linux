31a3f8b3cac23a48355c19a08e194395f10d61f4
gybpersist <3226299527@qq.com>
2025-10-13 10:43:29 +0800

    add:make_text

---
+++ test/make_test/hello.c
@@ -0,0 +1,11 @@
+#include "hello.h"
+#include <stdio.h>
+
+/**
+ * @brief ¥Ú”°hello world
+ * 
+ */
+void hello(void)
+{
+    printf("hello world\n");
+}
---
+++ test/make_test/hello.h
@@ -0,0 +1,10 @@
+#ifndef __HELLO_H__
+#define __HELLO_H__
+
+/**
+ * @brief ¥Ú”°hello world
+ * 
+ */
+void hello(void);
+
+#endif /* __HELLO_H__ */
