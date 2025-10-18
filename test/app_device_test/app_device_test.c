c21343f3bac9f272af8c84955e34146907314329
gybpersist <3226299527@qq.com>
2025-10-18 16:06:14 +0800

    fix:app_device_test

--- test/app_device_test/app_device_test.c
+++ test/app_device_test/app_device_test.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char *argv[])
 {
-    
+    // 使用 echo -n -e "\x01\x02\x03\x58\x58\x61\x62\x63" >>serial_file 在终端写入数据到serial_file中
     app_device_init("/home/ciqi9/桌面/Smart_Gateway_Linux/serial_file");
     app_device_start();
 
